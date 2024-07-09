340ba382cf67d020bdfa4e4ae7b59a21741a052f
PROMEGAMAX <lexa.pedskyshkin@mail.ru>
2024-07-10 01:31:30 +0700

    finished seven home work

---
+++ 7 work/main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+void print_menu() {
+    printf("ГЛАВНОЕ МЕНЮ\n");
+    printf("Выберите операцию\n");
+    printf("1. Сложение\n");
+    printf("2. Вычитание\n");
+    printf("3. Умножение\n");
+    printf("4. Деление\n");
+    printf("5. Выход\n");
+}
+
+int main(){
+    int div(int a, int b);
+    int sub(int a, int b);
+    int mul(int a, int b);
+    int add(int a, int b);
+    int choice = 0;
+    int num1, num2;
+
+    while(choice != 5){
+        print_menu();
+        scanf("%d", &choice);
+        if (choice == 5){
+            printf("Выход из программы\n");
+            break;
+        }
+        printf("Введите два числа: ");
+        scanf("%d %d", &num1, &num2);
+
+    switch(choice){
+        case 1:
+        printf("Результат: %d\n", add(num1, num2));
+        break;
+        case 2:
+        printf("Результат: %d\n", sub(num1, num2));
+        break;
+        case 3:
+        printf("Результат: %d\n", mul(num1, num2));
+        break;
+        case 4:
+        if(num2 != 0){
+            printf("Результат: %d\n", div(num1, num2));
+            }
+        else printf("Ошибка: Деление на ноль\n");
+        break;
+        default:
+        printf("Неверный ввод\n");
+    }
+}
+        return 0;
+}
